064c1e744cd240546a83efcaf5cdead8eaf24361
Lucas Caceres <lucas.caceres.lc@hotmai.com>
2023-05-25 23:49:40 -0300

    iniciar aplicacion

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,166 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+interface kingdomData {
+  kingdomId: string;
+  total: number;
+  name: string;
+  continent: number;
+}
+
+interface EnumKingdom extends Array<kingdomData> {}
 
 export default function Home() {
+  const [fecha, setFecha] = useState({
+    start: "",
+    end: "",
+    id: "",
+  });
+  const [data, setData] = useState<EnumKingdom>();
+  const [message, setMessage] = useState("");
+
+  const handleChange = ({
+    target: { name, value },
+  }: {
+    target: { name: string; value: string };
+  }) => {
+    setFecha((prevProduct) => ({ ...prevProduct, [name]: value }));
+  };
+
+  const ClearSearch = () => {
+    setFecha({
+      start: "",
+      end: "",
+      id: "",
+    });
+    setData([]);
+    setMessage("");
+  };
+
+  const GetData = async (id: string, startDate: string, endDate: string) => {
+    try {
+      const res = await fetch(
+        `https://api-lok-live.leagueofkingdoms.com/api/stat/land/contribution?landId=${id}&from=${startDate}&to=${endDate}`
+      );
+      const json = await res.json();
+      console.log(json);
+      if (json.result === true) {
+        setData(json.contribution);
+      }
+      if (
+        json.err.message ===
+        "Cast to date failed for value 'Invalid' Date at path 'logDate' for model 'LogLandDev'"
+      ) {
+        setMessage("Fecha invalida o indefinida");
+      }
+      if (
+        json.err.message ===
+        "Cast to number failed for value 'NaN' at path 'landId' for model 'Land'"
+      ) {
+        setMessage("ID No valido o inexistente");
+      }
+      if (json.err.code === "under 7 days") {
+        setMessage("La fecha no debe ser mayor 7 dias");
+      } else {
+        setMessage("Ocurrio un error, intentelo de nuevo");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+    <main className="flex min-h-screen flex-col items-center gap-12 p-6 md:p-12 lg:p-24 lg:pt-4 pt-4 md:pt-4">
+      <nav className="w-full flex justify-start">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          width={200}
+          height={200}
+          alt="logo"
+          src={"/assets/DRAGON_GAME.png"}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      </nav>
+      <section className="flex flex-col md:flex-row gap-4">
+        <input
+          type="text"
+          name="id"
+          placeholder="Ingrese su id"
+          onChange={handleChange}
+          value={fecha.id}
+          className="border-gradient text-neutral-400 rounded-sm p-2 bg-inherit outline-none"
+        />
+        <input
+          type="date"
+          name="start"
+          onChange={handleChange}
+          value={fecha.start}
+          className="border-gradient text-neutral-400 rounded-sm p-2 bg-inherit outline-none"
+        />
+        <input
+          type="date"
+          name="end"
+          onChange={handleChange}
+          value={fecha.end}
+          className="border-gradient text-neutral-400 rounded-sm p-2 bg-inherit outline-none"
+        />
+        <button
+          onClick={() => GetData(fecha.id, fecha.start, fecha.end)}
+          className="text-white rounded-sm p-2 bg-blue-700 active:bg-blue-800 bg-gradient-to-tr from-fuchsia-600 to-pink-500"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
+          Buscar
+        </button>
+        <button
+          onClick={() => ClearSearch()}
+          className="text-white rounded-sm p-2 bg-blue-700 active:bg-blue-800 bg-gradient-to-r from-fuchsia-600 to-pink-500"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          Limpiar busqueda
+        </button>
+      </section>
+      <div className="w-full">
+        <table className="w-full xl:w-10/12 m-auto">
+          <thead className="text-zinc-200">
+            <th className="p-4 xl:p-10 text-left border-y border-neutral-500 w-48">
+              Kingdom ID
+            </th>
+            <th className="p-4 xl:p-10 text-left border-y border-neutral-500 w-48">
+              Total
+            </th>
+            <th className="p-4 xl:p-10 text-left border-y border-neutral-500 w-48">
+              Name
+            </th>
+            <th className="p-4 xl:p-10 text-left border-y border-neutral-500 w-48">
+              Continent
+            </th>
+          </thead>
+          <tbody>
+            {data ? (
+              data.length > 0 &&
+              data.map((e, index) => {
+                return (
+                  <tr key={index}>
+                    <td className="p-4 xl:p-10 w-full xl:w-48  border-y border-neutral-500 text-left text-xs sm:text-sm">
+                      {e.kingdomId}
+                    </td>
+                    <td className="p-4 xl:p-10 w-full xl:w-48  border-y border-neutral-500 text-left text-xs sm:text-sm">
+                      {e.total}
+                    </td>
+                    <td className="p-4 xl:p-10 w-full xl:w-48  border-y border-neutral-500 text-left text-xs sm:text-sm">
+                      {e.name}
+                    </td>
+                    <td className="p-4 xl:p-10 w-full xl:w-48  border-y border-neutral-500 text-left text-xs sm:text-sm">
+                      {e.continent}
+                    </td>
+                  </tr>
+                );
+              })
+            ) : (
+              <th colSpan={4} className="text-center p-10 m-auto text-red-500">
+                {message ? message : "Sin resultados"}
+              </th>
+            )}
+          </tbody>
+        </table>
       </div>
     </main>
-  )
+  );
 }
